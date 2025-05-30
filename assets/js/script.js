48cacd6df63242b72be759ca882d75b1847fa7fc
Diego Garcia <garciadiegogabriel.gn@gmail.com>
2025-05-30 11:06:29 -0300

    MODIFIQUÉ LA ESTRUCTURA HTML Y REALICE EL JS DESDE 0

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const buscarImput = document.querySelector("form-control me-2");
+const btnBuscar = document.querySelector("btn btn-warning");
+const btnLimpiar = document.getElementById("btn btn-success");
+const contenedorMensajes = document.getElementById("message");
+const contenedorDePersonajes = document.getElementById("personajes"); 
+
+
+
+
